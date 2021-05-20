e24cad470010ab6ab0fdeb326a400511ef03b33f
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-05-20 21:16:20 +0700

    Update index.js

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/NavBar';
 
 const Layout = ({ children }) => {
 	return (
