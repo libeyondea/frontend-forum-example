74b462eb9b4490cab2ccf1ee210e939199e3db84
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-06-19 14:27:17 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -30,7 +30,7 @@ const App = ({ Component, pageProps }) => {
 	return (
 		<>
 			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
 			</Head>
 			<TopProgressBar />
 			<SWRConfig
