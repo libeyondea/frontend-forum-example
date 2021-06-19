74643e5ae22438bb7998328830e92d04e01c6fde
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-06-19 14:26:29 +0700

    Update index.js

--- src/modules/postCard/components/index.js
+++ src/modules/postCard/components/index.js
@@ -21,8 +21,8 @@ const PostCardComponent = ({ post }) => {
 								<CustomImage
 									src={`${process.env.IMAGES_URL}/${post.user.avatar}`}
 									className="rounded-circle h-100 w-100"
-									width={35}
-									height={35}
+									width={33}
+									height={33}
 									alt={post.user.user_name}
 								/>
 							</CustomLink>
