6f2e3ceb6ea99c4d0cdd5f2c9813481300156182
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-06-19 14:40:31 +0700

    Fix

--- src/common/components/ImageUserForm/components/index.js
+++ src/common/components/ImageUserForm/components/index.js
@@ -30,7 +30,7 @@ const ImageUserFormComponent = ({ errors, error, touched, label, imageSrc, imagA
 				</div>
 				{imageSrc && (
 					<div className="d-flex">
-						<CustomImage className="rounded-circle" src={`${imageSrc}h`} width={60} height={60} alt={imagAlt} />
+						<CustomImage className="rounded-circle" src={`${imageSrc}`} width={60} height={60} alt={imagAlt} />
 					</div>
 				)}
 			</div>
