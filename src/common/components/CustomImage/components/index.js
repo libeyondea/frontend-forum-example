cb0e3bf05e22156ebdff7567426d69519e3bb556
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-06-19 14:25:25 +0700

    Update next 11

--- src/common/components/CustomImage/components/index.js
+++ src/common/components/CustomImage/components/index.js
@@ -1,7 +1,10 @@
 import Image from 'next/image';
 
-const CustomImageComponent = ({ src, alt, className, ...props }) => (
-	<Image {...props} src={src} alt={alt} className={className} />
-);
+const CustomImageComponent = ({ src, alt, className, ...props }) =>
+	props.width <= 40 || props.height <= 40 ? (
+		<Image {...props} src={src} alt={alt} className={className} />
+	) : (
+		<Image {...props} src={src} alt={alt} className={className} blurDataURL={src} placeholder="blur" />
+	);
 
 export default CustomImageComponent;
