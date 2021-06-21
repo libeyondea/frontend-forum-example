fd0460b3809128291694bd6f34ce6e824b1f5cf9
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-06-21 17:59:29 +0700

    Update index.js

--- src/modules/home/languages/index.js
+++ src/modules/home/languages/index.js
@@ -1,11 +1,11 @@
 import * as en from '@/modules/home/languages/en';
-import * as vn from '@/modules/home/languages/vn';
+import * as vi from '@/modules/home/languages/vi';
 
 const Language = {
 	titleListPost: (language) => {
 		switch (language) {
 			case 'vi':
-				return vn.TEXT_TITLE_LIST_POST;
+				return vi.TEXT_TITLE_LIST_POST;
 			default:
 				return en.TEXT_TITLE_LIST_POST;
 		}
