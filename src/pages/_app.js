e2ff650031e6973a9311b2edeb9953ad1b1c590b
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-06-25 12:46:55 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -45,7 +45,7 @@ const App = ({ Component, pageProps }) => {
 						return error.response;
 					},
 					onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
-						if (error.response.status === 404 || key === '/current_user' || retryCount > 1) return;
+						if (error?.response?.status === 404 || key === '/current_user' || retryCount > 1) return;
 						setTimeout(() => revalidate({ retryCount }), 5000);
 					}
 				}}
