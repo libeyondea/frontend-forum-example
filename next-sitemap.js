c21425f6ca3a51e173c09a4c8b1c0879886bfff0
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-06-27 13:31:12 +0700

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,8 +1,8 @@
 module.exports = {
-	siteUrl: process.env.WEBSITE_URL,
+	siteUrl: process.env.WEBSITE_URL || 'https://de4thzone.com',
 	generateRobotsTxt: true,
 	exclude: ['/sitemap.xml'],
 	robotsTxtOptions: {
-		additionalSitemaps: [`${process.env.WEBSITE_URL}/sitemap.xml`]
+		additionalSitemaps: [`${process.env.WEBSITE_URL || 'https://de4thzone.com'}/sitemap.xml`]
 	}
 };
