d7d0148ed9b994c128b12899a8ba18b128389694
nhtanonymous <57558120+de4th-zone@users.noreply.github.com>
2021-07-09 12:49:35 +0700

    Update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,6 +21,8 @@ const TopProgressBar = dynamic(
 	{ ssr: false }
 );
 
+console.log('%cThis is a demo site', 'font-size: 4rem; color: red; font-weight: 600;');
+
 const App = ({ Component, pageProps }) => {
 	const router = useRouter();
 
